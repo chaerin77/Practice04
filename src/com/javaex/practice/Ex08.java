b754fc4cb292e1a8c92caf50c4099250fd31fc48
chaerin <77728068+Boram8878@users.noreply.github.com>
2021-11-21 23:44:54 +0900

    chapter04 ~Ex08

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -4,13 +4,13 @@ public class Ex08 {
 
 	public static void main(String[] args) {
 
-		int[] num = new int [6]; //방의 개수
+		int[] num = new int [6]; 
 		
 		for(int i=0; i<6; i++) {
 			
 			num[i]= (int)(Math.random()*45)+1;
 			
-			for(int y=0; y<i ; y++) {
+			for(int y=0; y<i ; y++) { 
 				
 				while(num[i]==num[y]) {
 					
